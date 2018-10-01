9b48a5a94e9e947c61d31e6d89f8965b785b60f0
Aidan Li <ali0100@myduneland.org>
2018-10-01 12:28:35 -0500

    x

---
+++ NestingDoll.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class NestingDoll here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NestingDoll
+{
+    private String name; private int layers;
+    
+    public NestingDoll(){name=""; layers=0;}
+    public NestingDoll(String nme, int lrs){name=nme; layers=lrs;}
+    public void clop(){System.out.println("Clip clop, clip clop");}
+    public String toString(){return "Russian nesting doll "+name+" has "+
+        layers+" layers.";}
+}
